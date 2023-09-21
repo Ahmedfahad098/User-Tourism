e83829d154204d10bf873438939bd41317ff2e2f
Ahmed <144206717+Ahmedfahad098@users.noreply.github.com>
2023-09-21 12:48:20 +0500

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './assets/components/Home/Footer'
 import Header from './assets/components/Home/Header'
+<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Activity } from './pages/Activity'
@@ -11,11 +12,21 @@ import { About } from './pages/About'
 import { Contact } from './pages/Contact'
 import Login from './assets/components/LoginSignup/Login'
 import SignUp from './assets/components/LoginSignup/SignUp'
+=======
+import ActivityMain from './assets/components/Activity/ActivityMain'
+import ActivityCardSec from './assets/components/Activity/ActivityCardSec'
+import DestinaCards from './assets/components/destination/DestinaCards'
+import CardsMain from './assets/components/packages/CardsMain'
+import AlTourCity from './assets/components/singleactivitypage/AlTourCity'
+
+
+>>>>>>> 2a051ed (oka)
 
 function App() {
 
   return (
     <>
+<<<<<<< HEAD
     {
       window.location.pathname !== "/login" && window.location.pathname !== "/signup" ? <Header/> : null
     }
@@ -35,6 +46,13 @@ function App() {
       window.location.pathname !== "/login" && window.location.pathname !== "/signup" ? <Footer/> : null
     }
     
+=======
+    <Header/>
+  
+  <ActivityMain/>
+   <AlTourCity/>
+    <Footer/>
+>>>>>>> 2a051ed (oka)
     </>
   )
 }
